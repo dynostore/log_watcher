238f6b44be8d414affbc085e095a67d50fa95d30
ddomizzisg <ddomizzisag@gmail.com>
2026-06-19 11:21:38 -0600

    fix: add logging for processing log lines to aid in debugging malformed entries

--- logwatcher/log_watcher.py
+++ logwatcher/log_watcher.py
@@ -277,6 +277,7 @@ def process_log_line(line: str):
     parts = line.split(',')
     
     ## ToDo: handle malformed log lines more robustly this is just a turn around to avoid crashes during development
+    print("Processing log line:", line)
     if not "dynostore" in line and len(parts) < 8:
         print("Malformed log line:", line)
         return
